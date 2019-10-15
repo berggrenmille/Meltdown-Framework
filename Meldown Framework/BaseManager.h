941ef1fb6424e18a068ddc9789dc1489a8804781
iceklue <iceklue@gmail.com>
2019-10-15 21:11:37 +0200

    Fixed typo in project name :D

--- Meldown Framework/BaseManager.h
+++ Meldown Framework/BaseManager.h
@@ -3,6 +3,9 @@ namespace Meltdown
 {
 	class Core; //Forward decl.
 
+	/// <summary>
+	/// BaseManager is the Base Class all managers inherits from, it grants access to the Meltdown::Core class.
+	/// </summary>
 	class BaseManager
 	{
 	public:
